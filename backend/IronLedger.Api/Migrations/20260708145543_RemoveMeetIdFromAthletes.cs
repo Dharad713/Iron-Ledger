67d2696b4590285249f13b35a11489d205bbd5d5
Dharad713 <dharad713@gmail.com>
2026-07-08 11:09:02 -0400

    migration

---
+++ backend/IronLedger.Api/Migrations/20260708145543_RemoveMeetIdFromAthletes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IronLedger.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMeetIdFromAthletes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MeetId",
+                table: "Athletes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MeetId",
+                table: "Athletes",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
