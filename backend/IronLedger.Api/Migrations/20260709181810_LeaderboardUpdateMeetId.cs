99d5a0f4ae03cc8ef8bb719da02a76979a50ced9
Dharad713 <dharad713@gmail.com>
2026-07-09 14:18:51 -0400

    feat: add meetId to leaderboard
    We obviously want to use the leaderboard service to see whos winning the meet

---
+++ backend/IronLedger.Api/Migrations/20260709181810_LeaderboardUpdateMeetId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IronLedger.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class LeaderboardUpdateMeetId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MeetId",
+                table: "Leaderboard",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MeetId",
+                table: "Leaderboard");
+        }
+    }
+}
