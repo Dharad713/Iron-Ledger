67467df340c88d27267b65700eb468931ce56691
Dharad713 <dharad713@gmail.com>
2026-07-16 13:08:05 -0400

    grpc: move Sex enum to common proto file

---
+++ backend/Services/IronLedger.AthleteService/Services/AthleteService.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Core;
+using IronLedger.Contracts.Athletes;
+using IronLedger.Contracts.Meets;
+
+namespace IronLedger.AthleteService.Services;
+
+
+public class AthleteService: Contracts.Athletes.AthleteService.AthleteServiceBase
+{
+
+}
